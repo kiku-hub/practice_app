d18733613532a0a32352a078d537e4a916a671ed
kiku-hub <tacyskani@gmail.com>
2024-11-08 17:21:50 +0900

    イベント処理の実装

--- src/pages/rsp/index.jsx
+++ src/pages/rsp/index.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 export default class Index extends React.Component {
   render() {
+    const imgSize = 100;
     return (
       <div
         style={{
@@ -12,8 +13,53 @@ export default class Index extends React.Component {
         }}
       >
         <h1>じゃんけんページ</h1>
+        <div style={{ display: "flex" }}>
+          <button
+            onClick={() => {
+              console.log("グーがクリックされました");
+            }}
+          >
+            <img
+              src={`images/rock.jpeg`}
+              alt="rock"
+              style={{
+                height: imgSize,
+                width: imgSize,
+              }}
+            />
+          </button>
+          <button
+            onClick={() => {
+              console.log("パーがクリックされました");
+            }}
+          >
+            <img
+              src={`images/paper.jpeg`}
+              alt="paper"
+              style={{
+                height: imgSize,
+                width: imgSize,
+              }}
+            />
+          </button>
+          <button
+            onClick={() => {
+              console.log("チョキがクリックされました");
+            }}
+          >
+            <img
+              src={`images/scissors.jpeg`}
+              alt="scissors"
+              style={{
+                height: imgSize,
+                width: imgSize,
+              }}
+            />
+          </button>
+        </div>
         <Link to="/">
           <h1>じゃんけんを終了する</h1>
+          <h4>※戦績が発表されます。</h4>
         </Link>
       </div>
     );
