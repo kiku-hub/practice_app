0de00c7e3f5ecf7b426756c6d7dabc3fe7e676d8
kiku-hub <tacyskani@gmail.com>
2024-11-08 15:22:08 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"; // Reactライブラリをインポート
 
-function App() {
+function App() { // Appという関数コンポーネントを定義
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> // divタグを返す
+      <h1>Hello World</h1> // h1タグで "Hello World" を表示
     </div>
   );
 }
 
-export default App;
+export default App; // Appコンポーネントをエクスポートし、他のファイルで使用可能にする
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'; // Reactライブラリをインポート
+import ReactDOM from 'react-dom/client'; // ReactDOMをインポートし、DOM操作に使用
+import './index.css'; // グローバルなCSSファイルをインポート
+import App from './App'; // メインコンポーネントAppをインポート
+import reportWebVitals from './reportWebVitals'; // パフォーマンス測定用の関数をインポート
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')); // idがrootのDOM要素にReactのルートを作成
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+); // React.StrictModeでAppコンポーネントをレンダリング
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(); // パフォーマンス測定を実行するための関数を呼び出し
